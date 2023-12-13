67afbbf5ebbb2759e6499d8f3c600582235ba179
laura Gaber <gaberlaura@lauras-MacBook-Pro.local>
2023-12-13 14:28:38 -0600

    created sign up component, form component, and button component

--- tutoring-app/src/utils/firebase/firebase.utils.js
+++ tutoring-app/src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
     getAuth, 
     signInWithRedirect, 
     GoogleAuthProvider,
-    signInWithPopup
+    signInWithPopup,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
     getFirestore,
@@ -36,7 +37,9 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async(userAuth) =>{
+export const createUserDocumentFromAuth = async(userAuth, additionalInformation = {}) =>{
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
     console.log(userDocRef);
 
@@ -54,15 +57,20 @@ export const createUserDocumentFromAuth = async(userAuth) =>{
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         }catch (error){
             console.log(`error creating the user`, error.message);
         }
     }
-
+    //if user data exists, return userDocRef
     return userDocRef;
-    //if user data exists
-    //return userDocRef
+    
+}
 
+export const createAuthUserWithEmailAndPassword = async(email, password) => {
+    if(!email || !password) return;
+
+    createUserWithEmailAndPassword(auth, email, password)
 }
